689e040351f180474a003e84663f1320b457952b
salta-ak <51148660+amsa10@users.noreply.github.com>
2025-03-17 12:14:29 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.detector import process_bulk_data
+
+# Define file paths
+INPUT_FILE = "data/input.csv"
+OUTPUT_FILE = "data/processed_results.csv"
+
+if __name__ == "__main__":
+    process_bulk_data(INPUT_FILE, OUTPUT_FILE)
